5357f00bf667ea3df3715184fbe480371aaa74a5
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-04-07 08:30:24 +0300

    intial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// Your code here
+document.addEventListener("DOMContentLoaded", (event)=>{
+
+})
+//declare variables for DOM manipulation.
+const movieList = document.getElementBy("films");
+const poster = document.getElementById("poster");
+const movieTitle =document .getElementById("title");
+const runTime = document.getElementById(`"${runtime}  minutes"`);
+const movieDescription = document.getElementById("film-info")
+const showTime = document.getElementById("showtime");
+// let ticketsRemaining = document.getElementById("ticket-num");
+// ticketsRemaining = `(${capacity})- (${ticket_sold})`
+const buyTicketBtn = document.getElementById("buy-ticket");
+// fetched data goes here
+let movie = [];
+
